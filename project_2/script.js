134fc6493a296ea0eb14947bf3616a5f9b63aade
Aman <amanraikwar1996@gmail.com>
2026-02-20 15:35:31 +0530

    Added GSAP animations and ScrollTrigger effects for page 3 boxes.

--- project_2/script.js
+++ project_2/script.js
@@ -24,29 +24,54 @@ gsap.to("#page2 h1",{
         end:"top -120%",
         scrub:2,
         pin:true,
-        pinSpacing:false
     }
 })
 
 gsap.to("#box1",{
-    y:"80vh",
-    duration:5,
-    delay:2,
+    x:1000,
+    duration:3,
+    delay:1,
     borderRadius:"50%",
     backgroundColor:"rgb(237, 132, 4)",
+    borderRadius:"50%",
     repeat:-1,
     yoyo:true,
     rotate:360,
+    scrollTrigger:{
+        trigger:"#page3",
+        start:"top 80%",
+    }
     
 })
 
 gsap.from("#box2",{
-    y:"80vh",
-    duration:5,
-    delay:2,
-    borderRadius:"50%",
+    x:1000,
+    duration:3,
+    delay:1,
+    borderRadius:"0%",
     backgroundColor:"rgb(237, 132, 4)",
     repeat:-1,
     yoyo:true,
     rotate:360,
+    scrollTrigger:{
+        trigger:"#page3",
+        start:"top 80%",
+    }
+})
+
+gsap.to("#box3",{
+    x:1000,
+    duration:3,
+    delay:1,
+    borderRadius:"50%",
+    backgroundColor:"rgb(237, 132, 4)",
+    borderRadius:"50%",
+    repeat:-1,
+    yoyo:true,
+    rotate:360,
+    scrollTrigger:{
+        trigger:"#page3",
+        start:"top 80%",
+    }
+    
 })
