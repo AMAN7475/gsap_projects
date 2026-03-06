f1306d1f928c937dedff942d55b52656ccde06b6
Aman <amanraikwar1996@gmail.com>
2026-03-06 19:23:11 +0530

    Project react_with_gsap: Initialize React project with GSAP integration.

---
+++ react_with_gsap/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { useGSAP } from "@gsap/react";
+import gsap from 'gsap';
+
+const App = () => {
+  gsap
+  useGSAP(()=>{
+
+  })
+
+  return (
+    <main>
+      <div className="box">
+
+      </div>
+    </main>
+  )
+}
+
+export default App
