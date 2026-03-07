ea5e1eee1123fcdd4e00868ded8bfb2a4114fe8a
Aman <amanraikwar1996@gmail.com>
2026-03-07 19:56:14 +0530

    feat: Integrate useState hook and GSAP utilities for basic animations.

--- react_with_gsap/src/App.jsx
+++ react_with_gsap/src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useRef } from 'react'
 import { useGSAP } from "@gsap/react";
 import gsap from 'gsap';
+import { useState } from 'react';
 
 const App = () => {
 
@@ -26,28 +27,33 @@ const App = () => {
   //   })
   // },{scope:".container"})
 
-    const container = useRef()
-    useGSAP(()=>{
-    gsap.from(".box",{
-      rotate:360,
-      duartion:10,
-      delay:1,
-      opacity:0,
-      y: 500
+  // const container = useRef()
+  // useGSAP(()=>{
+  //   gsap.from(".box",{
+  //     rotate:360,
+  //     duartion:10,
+  //     delay:1,
+  //     opacity:0,
+  //     y: 500
+  //   })
+  // },{scope:container})
+
+  const [circle, setCircle] = useState(0)
+  const random = gsap.utils.random(-500,500,10)
+
+  useGSAP(()=>{
+    gsap.to(".circle",{
+      x:circle,
+      duration:0.5
     })
-  },{scope:container})
+  },[circle])
 
   return (
     <main>
-      <div ref={container} className="container">
-        <div className="circle"></div>
-        <div className="box"></div>
-      </div>
-
-      <div className="container2">
-        <div className="circle"></div>
-        <div className="box"></div>
-      </div>
+      <button onClick={()=>{
+          setCircle(random)
+      }}>Animate</button>
+      <div className="circle"></div>
     </main>
   )
 }
